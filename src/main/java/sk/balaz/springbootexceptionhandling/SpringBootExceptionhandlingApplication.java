4ca158e9c8b067d01b0323249b9034e8ef0d5c0b
Tomas Balaz <tomasba@amdocs.com>
2022-06-01 14:10:52 +0200

    Created spring-boot-exceptionhandling application

---
+++ src/main/java/sk/balaz/springbootexceptionhandling/SpringBootExceptionhandlingApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.springbootexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptionhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExceptionhandlingApplication.class, args);
+	}
+
+}
