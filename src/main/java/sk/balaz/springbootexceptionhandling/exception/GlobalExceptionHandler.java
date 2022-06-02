1621b3231acfa30f9ee4980ccd764dceb8612b03
Tomas Balaz <tomasba@amdocs.com>
2022-06-02 13:48:29 +0200

    Example how to handle SpringMVC Exceptions by extending ResponseEntityExceptionHandler

--- src/main/java/sk/balaz/springbootexceptionhandling/exception/GlobalExceptionHandler.java
+++ src/main/java/sk/balaz/springbootexceptionhandling/exception/GlobalExceptionHandler.java
@@ -4,12 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import sk.balaz.springbootexceptionhandling.customer.CustomerNotFoundException;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomerNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException e) {
