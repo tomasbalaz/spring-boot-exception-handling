c14b51cf0de77e3bbb5e745494c2e4f49161bba3
Tomas Balaz <tomasba@amdocs.com>
2022-06-02 14:15:30 +0200

    Example how to handle SpringMVC Exceptions with HTTP response body

--- src/main/java/sk/balaz/springbootexceptionhandling/exception/GlobalExceptionHandler.java
+++ src/main/java/sk/balaz/springbootexceptionhandling/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package sk.balaz.springbootexceptionhandling.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import sk.balaz.springbootexceptionhandling.customer.CustomerNotFoundException;
 
@@ -18,9 +21,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        ErrorResponse response = new ErrorResponse(e.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    public ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), LocalDateTime.now());
+        return this.handleExceptionInternal(ex, response, headers, status, request);
     }
 }
