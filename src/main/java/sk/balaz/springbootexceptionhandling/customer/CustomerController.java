bc37b57b44e0d2f2cd031da16bb28886c1f3421c
Tomas Balaz <balaztomas0@gmail.com>
2023-01-17 15:57:27 +0100

    Updated pom.xml with problem-spring-web-starter dependency and removed all exception handlers

--- src/main/java/sk/balaz/springbootexceptionhandling/customer/CustomerController.java
+++ src/main/java/sk/balaz/springbootexceptionhandling/customer/CustomerController.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sk.balaz.springbootexceptionhandling.exception.ErrorResponse;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -32,11 +30,5 @@ public class CustomerController {
     @ResponseStatus(HttpStatus.CREATED)
     public Customer createCustomer(@RequestBody @Valid Customer customer) {
         return customerService.createCustomer(customer);
-    }
-
-    @ExceptionHandler(CustomerNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException e) {
-        ErrorResponse response = new ErrorResponse(e.getMessage(), LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }
