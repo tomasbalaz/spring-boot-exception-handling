7c4ebe5738de68e6fdbca99e0f5c6f4c6222b35b
Tomas Balaz <balaztomas0@gmail.com>
2023-01-17 16:02:31 +0100

    When CustomerNotFoundException does not have @ResponseStatus annotation then Spring throws HTTP 500 error code

--- src/main/java/sk/balaz/springbootexceptionhandling/customer/CustomerNotFoundException.java
+++ src/main/java/sk/balaz/springbootexceptionhandling/customer/CustomerNotFoundException.java
@@ -3,7 +3,7 @@ package sk.balaz.springbootexceptionhandling.customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException {
 
     public CustomerNotFoundException(String message) {
